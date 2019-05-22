1e78993066868422e8abe777e619d6424fb77c72
CataMZ <catarina.manzieri@gmail.com>
2019-05-22 19:52:39 -0300

    Terminar fetch da API

--- js/script.js
+++ js/script.js
@@ -6,14 +6,36 @@ fetch(`https://theblackwomanhistory.firebaseio.com/.json`)
     data.content.forEach(perfil => {
         const box = document.querySelector(".maravilhosas__box");
 
-        const div = document.createElement('div');
-        div.setAttribute('class', 'maravilhosas__perfil');
+        const card = document.createElement('div');
+        card.setAttribute('class', 'maravilhosas__perfil');
 
-        const clic = document.createElement('a');
+        const profile = document.createElement('a');
+        profile.setAttribute('href', '#')
 
+        const img = document.createElement('img');
+        img.setAttribute('class', 'img-responsive');
 
-        box.appendChild(div);
-        div.appendChild(clic);
-        clic.appendChild(a);
+        if (perfil.metadata){
+            if (perfil.metadata.image){
+                if (perfil.metadata.image.url){
+                    img.setAttribute('src', perfil.metadata.image.url);
+                }
+            } else {
+                img.setAttribute('src', './img/img-mulher.png');
+            }
+        } else {
+            img.setAttribute('src', './img/img-mulher.png');
+        }
+
+        const name = document.createElement('p');
+        name.textContent = perfil.title;
+
+        box.appendChild(card);
+        card.appendChild(profile);
+        profile.appendChild(img);
+        profile.appendChild(name);
     })
+})
+.catch(function(erro){
+    console.log(erro);
 })
